0eb06ad684c78eeb900c6b5b343303b1f8e89652
jeremya-lagash <jeremya@lagash.com>
2020-04-06 13:19:34 -0400

    paginacion

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,25 +1,32 @@
 import React from "react";
+import { MemoryRouter, Route } from "react-router";
+import { Link } from "react-router-dom";
+import Pagination from "@material-ui/lab/Pagination";
+import PaginationItem from "@material-ui/lab/PaginationItem";
 
-const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
-  const pageNumbers = [];
-
-  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
-    pageNumbers.push(i);
-  }
-
+export default function PaginationLink() {
   return (
-    <nav>
-      <ul className="pagination">
-        {pageNumbers.map(number => (
-          <li key={number} className="page-item">
-            <a onClick={() => paginate(number)} href="!#" className="page-link">
-              {number}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  );
-};
+    <MemoryRouter initialEntries={["/inbox"]} initialIndex={0}>
+      <Route>
+        {({ location }) => {
+          const query = new URLSearchParams(location.search);
+          const page = parseInt(query.get("page"), 10) || 1;
 
-export default Pagination;
+          return (
+            <Pagination
+              page={page}
+              count={10}
+              renderItem={(item) => (
+                <PaginationItem
+                  component={Link}
+                  to={`/inbox${item.page === 1 ? "" : `?page=${item.page}`}`}
+                  {...item}
+                />
+              )}
+            />
+          );
+        }}
+      </Route>
+    </MemoryRouter>
+  );
+}
